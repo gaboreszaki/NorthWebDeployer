4631c9a7f034d15d01e079d7269d5b59168dd035
Mr. Gabor Eszaki <24547662+gaboreszaki@users.noreply.github.com>
2023-12-20 14:25:35 +0100

    v0.4.1 - Minor fix - Fix for storage

--- src/builder_laravel.sh
+++ src/builder_laravel.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 function builder() {
+  # Copy files to the destination
   cp -r "$GIT_temp_folder/$APP_build_date/" "$ACTIVE_version"
 
   cd "$ACTIVE_version" || exit
 
-  fixPermissionsForLaravel
-
+  # Build dependencies
   composer install
 
   npm install
   npm run build
 
+
+
+  #Creating Symlinks for .env and public storage
+  php artisan storage:link
   createSymlinkForEnv
+
+  fixPermissionsForLaravel
 
   php artisan cache:clear
   php artisan optimize
