342a078538acaa65b5c5b65363a187e28e95d684
Mr. Gabor Eszaki <24547662+gaboreszaki@users.noreply.github.com>
2023-12-20 14:50:21 +0100

    v0.4.2 - Feature dev env - added optional variable "$IS_DEPLOYMENT_PRODUCTION" to determinate migrations

--- src/builder_laravel.sh
+++ src/builder_laravel.sh
@@ -15,6 +15,16 @@ function builder() {
 
   php artisan cache:clear
   php artisan optimize
-  php artisan migrate
+  # If $IS_DEPLOYMENT_PRODUCTION is set and its false run dev migrations
+  # else run Production migrate
+  if [[ -v $IS_DEPLOYMENT_PRODUCTION && $IS_DEPLOYMENT_PRODUCTION = false ]] ; then
+
+      echo 'Be careful not to fall off!'
+      php artisan migrate:fresh --seed
+
+      else
+      php artisan migrate
+
+  fi
 
 }
